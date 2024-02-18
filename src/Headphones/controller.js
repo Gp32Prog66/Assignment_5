7903e9a4e88b5f511c54bceed1c3e13eeaeaf56d
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-02-18 14:02:09 -0500

    Update controller.js
    Adding Controllers to retrieve headphones, headphonesID, addingHeadPhones, and updatingHeadPhones.

--- src/Headphones/controller.js
+++ src/Headphones/controller.js
@@ -12,6 +12,38 @@ const getHeadphones = (req, res) =>
     });
 };
 
+//Get Headphones by ID
+const getHeadPhonesById = (req, res) => 
+{
+    const id = parseInt(req.params.id);
+    pool.query(queries.getHeadPhonesById,[id], (error, results) =>
+    {
+        if(error) throw error;
+        res.status(200).json(results.rows);
+});
+
+}
+const addHeadPhones = (req, res) => 
+{
+    const {id, brand, model_number, wireless, noise_cancellation} = req.body;
+    pool.query(queries.addHeadPhones, [id, brand, model_number, wireless, noise_cancellation], (error, results)=> {
+    if(error) throw error;
+    res.status(200).send("Added New HeadPhones Item successfully");
+    });
+}
+
+//Update Inventory
+const updateHeadPhones = (req,res) =>
+{
+    //const id = parseInt(req.params.id);
+    const { id, brand } = req.body;  // May need to come back and modify
+    pool.query(queries.updateHeadPhones, [id,brand],(error,results) => 
+    {
+        if(error) throw error;
+        res.status(200).send("Updated Inventory Successfully");
+    })
+}
+
 //Getting Data by Parameter
 //Add new Data by Parameter
 //Modify Data by parameter
