3a090deee5729f5ca7b24a9ac968d349e6584922
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-02-18 14:29:43 -0500

    Update routes.js
    Adding Routes to get headphonesID and AddingHeadPhones

--- src/Headphones/routes.js
+++ src/Headphones/routes.js
@@ -4,5 +4,7 @@ const controller = require('./controller');
 const router = Router();
 
 router.get('/', controller.getHeadphones);
+router.get('/:id', controller.getHeadPhonesById);
+router.post('/', controller.addHeadPhones);
 
 module.exports = router;
