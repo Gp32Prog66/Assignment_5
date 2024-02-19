4c35587253038885cc99485a1b5e7067c766ac5a
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-02-19 15:50:14 -0500

    Update queries.js
    Adding More Queries to Retrieve Different Tables, Add Values, and Updated Values.

--- src/Headphones/queries.js
+++ src/Headphones/queries.js
@@ -1,13 +1,27 @@
 const getHeadPhones = "SELECT * from headphones"; //Changing getInventory to getHeadPhones
-const getHeadPhonesById = 'SELECT * FROM headphones WHERE id = $1";
+const getHeadPhonesById = "SELECT * FROM headphones WHERE id = $1";
+const getSonyBrandHeadPhones = "SELECT * FROM headphones WHERE id = $'Sony'";
+const getWirelessHeadPhones = "SELECT * FROM headphones WHERE wireless = $'TRUE'";
+
+
 const addHeadPhones = "INSERT into headphones (id,brand,model_number,wireless,noise_cancellation) VALUES($1, $2, $3, $4, $5)";
+const addFirstThreeColumns = "INSERT INTO headphones (id,brand,model_number) VALUES ($1,$2,$3)";
+
+
 const updateHeadPhones = "UPDATE headphones set id = $1 where brand = $2";
+const updateNoiseCancellation = "UPDATE headphones SET noisecancellation = $5 where id = $1";
+const updateWireless = "UPDATE headphones SET wireless $4 where id = $1";
 
 //Add Headphones
 
 module.exports = {
     getHeadPhones,
     getHeadPhonesByID,
+    getSonyBrandHeadPhones,
+    getWirelessHeadPhones,
     addHeadPhones,
-    updateHeadPhones
+    addFirstThreeColumns,
+    updateHeadPhones,
+    updateNoiseCancellation,
+    updateWireless
 };
