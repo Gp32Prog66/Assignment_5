fe989e1fe7a70858217f58e47bf1cc96c561d4c7
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-02-20 17:00:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res)=>
 })
 
 //API Route
-//http://localhost:8007/
+//http://localhost:8007//
 app.use("/api/v1/headphones", headPhonesRoutes);     //Check Here for any Issues
 
 app.listen(port, () => console.log('running on ${port}'));
