cdac5374888c44539c0243bd4b1be6d1b699fad1
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-02-19 22:19:47 -0500

    Update queries.js
    Changed A Few Things and Left Some Comments on My Changes

--- src/Headphones/queries.js
+++ src/Headphones/queries.js
@@ -1,16 +1,18 @@
 const getHeadPhones = "SELECT * from headphones"; //Changing getInventory to getHeadPhones
+//Query Different Tables
 const getHeadPhonesById = "SELECT * FROM headphones WHERE id = $1";
-const getSonyBrandHeadPhones = "SELECT * FROM headphones WHERE id = $'Sony'";
-const getWirelessHeadPhones = "SELECT * FROM headphones WHERE wireless = $'TRUE'";
+const getSonyBrandHeadPhones = "SELECT * FROM headphones WHERE id = $2"; //Changed From Sony 
+const getWirelessHeadPhones = "SELECT * FROM headphones WHERE wireless = $4"; //Changed From True
+//const getEarBuds = "SELECT * from headphones";
 
-
+//Add Values to HeadPhones
 const addHeadPhones = "INSERT into headphones (id,brand,model_number,wireless,noise_cancellation) VALUES($1, $2, $3, $4, $5)";
 const addFirstThreeColumns = "INSERT INTO headphones (id,brand,model_number) VALUES ($1,$2,$3)";
 
-
-const updateHeadPhones = "UPDATE headphones set id = $1 where brand = $2";
+//Update Values to HeadPhones
+const updateHeadPhones = "UPDATE headphones set id = $2 where brand = $1"; //ID = $2 not $1  brand = $1 not $2
 const updateNoiseCancellation = "UPDATE headphones SET noisecancellation = $5 where id = $1";
-const updateWireless = "UPDATE headphones SET wireless $4 where id = $1";
+const updateWireless = "UPDATE headphones SET wireless = $4 where id = $1";
 
 //Add Headphones
 
@@ -19,8 +21,11 @@ module.exports = {
     getHeadPhonesByID,
     getSonyBrandHeadPhones,
     getWirelessHeadPhones,
+    //getEarBuds,
+    
     addHeadPhones,
     addFirstThreeColumns,
+    
     updateHeadPhones,
     updateNoiseCancellation,
     updateWireless
