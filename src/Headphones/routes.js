e8f9bdd044323880ffd9e7739ba5ac1ff6af49da
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-02-19 22:13:01 -0500

    Update routes.js
    More Organized Routes

--- src/Headphones/routes.js
+++ src/Headphones/routes.js
@@ -3,12 +3,18 @@ const controller = require('./controller');
 
 const router = Router();
 
-router.get('/', controller.getHeadphones);
+//Get Routes
+router.get('/', controller.getHeadPhones);
+router.get('/:id', controller.getHeadPhonesById);
 router.get('/:brand', controller.getSonyBrandHeadPhones);
 router.get('/:wireless', controller.getWirelessHeadPhones);
-router.get('/:id', controller.getHeadPhonesById);
+//router.get('/', controller.getEarBuds);
+
+//Add Routes
 router.post('/', controller.addHeadPhones);
 router.post('/', controller.addFirstThreeColumns);
+
+//Update Routes
 router.put('/', controller.updateHeadPhones);
 router.put('/', controller.updateNoiseCancellation);
 router.put('/', controller.updateWireless);
