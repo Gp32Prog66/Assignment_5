5ce11d7343396c7b7c027696ab509db9a39164bf
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-03-05 17:48:16 -0500

    Update server.js
    Forgot to Add Earbud Routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 //Building New Routes
 const headPhonesRoutes = require("./src/Headphones/routes");
-
+const earBudsRoutes = require("./src/Earbuds/routes");
 
 const app = express();
 const port = 8007;
@@ -21,6 +21,8 @@ app.get("/", (req, res)=>
 //API Route
 //http://localhost:8007//
 app.use("/api/v1/headphones", headPhonesRoutes);     //Check Here for any Issues
+//Adding Earbuds Route
+app.use("/api/v1/earbuds", earBudsRoutes);
 
 app.listen(port, () => console.log('running on ${port}'));
 
